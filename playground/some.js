6a652181ac5d632447c4cb2c8525d6e2e0e93f79
imaffett23 <45928755+imaffett23@users.noreply.github.com>
2021-04-20 21:49:52 -0400

    JS_WB initial setup (ESLint, etc.)

---
+++ playground/some.js
@@ -0,0 +1,17 @@
+console.log("I'm in another file!");
+
+// JavaScript types
+//
+// SNOB'N'US
+//
+// STRING
+// NUMBER
+// BOOLEAN
+// NULL
+// UNDEFINED
+// SYMBOL
+//
+// OBJECT
+
+// First six are "primative" types
+// Object is a bit more special.  Most things in JS are objects
