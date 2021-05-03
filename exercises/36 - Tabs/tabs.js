adf443b58d710567eacdb515ffcb0b841726da64
imaffett23 <45928755+imaffett23@users.noreply.github.com>
2021-05-02 23:08:49 -0400

    JS_WB Tab Exercise & Flow Control

--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -1,1 +1,33 @@
-console.log('ya ya wes we get it.. IT WORKS!');
+const tabs = document.querySelector('.tabs');
+const tabButtons = tabs.querySelectorAll('[role="tab"]');
+const tabPanels = Array.from(tabs.querySelectorAll('[role="tabpanel"]'));
+
+function handleTabClick(event) {
+  // Hide all tab panels
+  tabPanels.forEach((panel) => {
+    panel.hidden = true;
+  });
+
+  // Mark all tabs as unselected
+  tabButtons.forEach((tab) => {
+    tab.setAttribute('aria-selcted', false); // Need to setAttribute for aria attributes.
+  });
+
+  // Mark this tab as selected
+  event.currentTarget.setAttribute('aria-selected', true);
+
+  // Find the associated tab panel and show it
+  const { id } = event.currentTarget;
+  // METHOD 1
+  //   const tabPanel = tabs.querySelector(`[aria-labelledby=${id}]`);
+  //   tabPanel.hidden = false;
+  // METHOD 2 - find in the array of tab panels
+  const tabPanel = tabPanels.find(
+    (panel) => panel.getAttribute('aria-labelledby') === id
+  );
+  tabPanel.hidden = false;
+}
+
+tabButtons.forEach((button) =>
+  button.addEventListener('click', handleTabClick)
+);
